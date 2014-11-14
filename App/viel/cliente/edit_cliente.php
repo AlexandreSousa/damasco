6e0fb694d43131c63fc16441758121514d1d4841
sousa.akira@gmail.com <watashi455ttte>
2014-11-14 12:12:16 -0200

    ...

--- App/viel/cliente/edit_cliente.php
+++ App/viel/cliente/edit_cliente.php
@@ -6,9 +6,22 @@
  * Time: 17:57
  */
 
-echo $id = $_GET['id'];
+$id = $_GET['id'];
 
 $mostra = $db->read('menu',"WHERE id = {$id}",'');
 
 $form = new Tform;
+$form->Tformi('form1');
+$form->setLabel('id_ramo');
+$form->Tinput('text','nome','id',$mostra[id_ramo],'','',20);
+$form->setLabel('id_level');
+$form->Tinput('text','nome','id',$mostra[id_level],'','',20);
+$form->setLabel('Icon');
+$form->Tinput('text','nome','id',$mostra[icon],'','',20);
+$form->setLabel('Nome');
 $form->Tinput('text','nome','id',$mostra[nome],'','',20);
+$form->setLabel('Status');
+$form->Tinput('text','nome','id',$mostra[status],'','',20);
+echo '<br>';
+$form->Tbuton('submit','Atualizar');
+$form->Tformf();
